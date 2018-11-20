daf99d537b1aee315d2520f678e59001e72defa5
atsuhiro-narita <alayashiki@gmail.com>
2018-11-20 13:19:43 +0900

    python ディレクトリを異動

--- python/queries/healthcheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-bq query --use_legacy_sql=False --dry_run=False 'select table, max(time) as latest_time, count(*) as numOf2, sum(ros) as row from (select * from trading.channel_minutely_rows limit 16) group by table order by table;'
-
